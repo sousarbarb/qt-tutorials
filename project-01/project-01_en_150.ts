a173570ece0a48cabdfa4d985752c6ad19262d78
Ricardo B. Sousa <sousa.ricardob@outlook.com>
2021-03-02 17:22:08 +0000

    Project 01: Only visualization and Qt Design

---
+++ project-01/project-01_en_150.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="project-01_en_150">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/project-01_autogen/include/ui_mainwindow.h" line="232"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <location filename="build/project-01_autogen/include/ui_mainwindow.h" line="233"/>
+        <source>Tuesday, 02-03-2021 4:03 PM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="250"/>
+        <location filename="build/project-01_autogen/include/ui_mainwindow.h" line="237"/>
+        <source>Username:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="267"/>
+        <location filename="build/project-01_autogen/include/ui_mainwindow.h" line="238"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <location filename="build/project-01_autogen/include/ui_mainwindow.h" line="239"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
